df3a8c070ef62a304f5f3d6755f7f9d621fb64e3
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-09-17 19:15:04 +0200

    Mise à jour de la mise en page de la page d'accueil et ajout des traductions en français

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,11 @@ import { getTranslations } from "next-intl/server";
 export default async function HomePage() {
 	const t = await getTranslations("HomePage");
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<h1>{t("title")}</h1>
+		<main className="p-16 flex min-h-screen flex-col items-center justify-between">
+			<div className="text-center flex flex-col gap-y-4">
+				<h1 className="text-5xl font-bold">{t("title")}</h1>
+				<h2 className="text-xl font-light">{t("tagLine")}</h2>
+			</div>
 		</main>
 	);
 }
